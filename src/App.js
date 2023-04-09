932727600583f4a5f267c42454702d82c8c06c5f
Fatih Erdoğan SEVİM <fatih.e.sevim@gmail.com>
2023-04-10 01:07:24 +0300

    added packages
    axios, bootstrap, moment, primereact, redux, sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Base from "./layout/base.jsx";
+
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+//core
+import "primereact/resources/primereact.min.css";
+//icons
+import "primeicons/primeicons.css";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <Base />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/layout/base.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Button } from "primereact/button";
+
+const Base = () => {
+    return (
+        <div className="row">
+            <div className="col-6">asd</div>
+            <div className="col-6">qwe</div>
+            <div className="col-3">
+                <Button>asd</Button>
+            </div>
+        </div>
+    );
+};
+
+export default Base;
