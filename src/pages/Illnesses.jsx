ccd89d219471700598592ba7906572510f44b60b
emredemiratan <emredemiratan@hotmail.com>
2023-05-25 03:30:37 +0300

    illnesses updated

--- src/pages/Illnesses.jsx
+++ src/pages/Illnesses.jsx
@@ -6,34 +6,40 @@ import { useNavigate } from "react-router-dom";
 import { Calendar } from "primereact/calendar";
 
 const Illnesses = () => {
-    const dispatch = useDispatch();
-    const { loading } = useSelector((state) => state.global);
-    const navigate = useNavigate();
-    const [date, setDate] = useState();
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.global);
+  const navigate = useNavigate();
+  const [date, setDate] = useState();
 
-    return (
-        <div className="flex h-full w-full items-center justify-center blue-bg">
-            <div className="flex flex-col items-center w-1/2 border-2 gray-bg border-gray-400 p-4 gap-4 rounded-lg">
-                <div className="flex flex-wrap justify-center">
-                    <div className="w-6/12 sm:w-4/12 px-4">
-                        <img
-                            src={require("../assets/medalert-logo.png")}
-                            alt="..."
-                            className="shadow rounded-full max-w-full h-auto align-middle border-none"
-                        />
-                    </div>
-                </div>
-                <div className="mx-auto w-1/2">
-                    <div className="flex flex-row items-end">
-                        <span>Illnesses</span>
-                    </div>
-                    <div className="flex flex-row mx-auto border-2 border-gray-400 p-4 gap-2 rounded-lg bg-teal-100">
-                        <span>Illnesses 1</span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex h-full w-full items-center justify-center blue-bg">
+      <div className="flex flex-col items-center w-1/2 border-2 gray-bg border-gray-400 p-4 gap-4 rounded-lg">
+        <div className="flex w-full justify-start items-center">
+          <button onClick={() => navigate("/myAccount")}>
+            <i className="pi pi-arrow-left mr-3" />
+            <span>My Account</span>
+          </button>
         </div>
-    );
+        <div className="flex flex-wrap justify-center">
+          <div className="w-6/12 sm:w-4/12 px-4">
+            <img
+              src={require("../assets/medalert-logo.png")}
+              alt="..."
+              className="shadow rounded-full max-w-full h-auto align-middle border-none"
+            />
+          </div>
+        </div>
+        <div className="mx-auto w-1/2">
+          <div className="flex flex-row items-end">
+            <span>Illnesses</span>
+          </div>
+          <div className="flex flex-row mx-auto border-2 border-gray-400 p-4 gap-2 rounded-lg bg-teal-100">
+            <span>Illnesses 1</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Illnesses;
